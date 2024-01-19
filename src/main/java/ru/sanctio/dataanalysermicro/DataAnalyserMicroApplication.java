188da5549ede52f0daa9e7e08c6aa41f8f65d804
Evgeniy Sharychenkov <sanctio@yandex.ru>
2024-01-20 02:14:12 +0300

    Initial commit

---
+++ src/main/java/ru/sanctio/dataanalysermicro/DataAnalyserMicroApplication.java
@@ -0,0 +1,13 @@
+package ru.sanctio.dataanalysermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyserMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyserMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sanctio/dataanalysermicro/DataAnalyserMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sanctio.dataanalysermicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyserMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
